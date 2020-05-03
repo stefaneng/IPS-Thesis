4d636919738c6f791851eb0474a8edd157d357cb
Stefan Eng <stefaneng13@gmail.com>
2020-05-03 09:55:38 +0200

    Side-by-side plot of voter model dual

--- interparsys/R/voter_model_dual.R
+++ interparsys/R/voter_model_dual.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(tidyr)
+library(ggpubr)
 
 #' Creates a visualzation of the voter model dual
 #' If pretty = TRUE, then does not actually use exponential distributions but attempt to "look" random
@@ -7,18 +8,35 @@ vm_dual_sim <- function(n = 4, pretty = FALSE, delta = .1) {
   k <- 2 * n + 1
   m <- round(runif(k))
 
+  # use lattice for x value centered at 0
   x_indices <- c(-n:-1, 0, seq_len(n))
-  # Poisson thinning/super...
-  event_n <- rpois(1, lambda = k)
-  event_time <- sort(rexp(event_n, rate = 1))
-  # Randomly select which Poisson process the event came from
-  event_i <- sample(1:k, size = event_n, replace = TRUE)
-  arrow_end <- event_i + sample(c(-1, 1), event_n, replace = TRUE)
-  # Flip arrows past k
-  arrow_end[arrow_end > k] <- k - 1
-  arrow_end[arrow_end == 0] <- 2
+  if(pretty) {
+    # Always have 1-3 events for a given lattice point
+    # Uniformly draw rather than exponential
+    event_counts <- sample(1:3, size = k, replace = TRUE)
+    tmp_arrow <- lapply(seq_along(event_counts), function(i) {
+      data.frame(event_time = runif(event_counts[i]),
+      event_i = i,
+      arrow_end = i + sample(c(-1, 1), event_counts[i], replace = TRUE))
+    })
+    arrow_df <- do.call(rbind, tmp_arrow)
+    arrow_df <- arrow_df[order(arrow_df$event_time), ]
+    event_n <- nrow(arrow_df)
+  } else {
+    # Poisson superposition
+    event_n <- rpois(1, lambda = k)
+    event_time <- sort(rexp(event_n, rate = 1))
+    # Randomly select which Poisson process the event came from
+    event_i <- sample(1:k, size = event_n, replace = TRUE)
+    arrow_end <- event_i + sample(c(-1, 1), event_n, replace = TRUE)
 
-  arrow_df <- data.frame(event_time, event_i, arrow_end)
+    arrow_df <- data.frame(event_time, event_i, arrow_end)
+  }
+
+  # Flip arrows past k
+  arrow_df$arrow_end[arrow_df$arrow_end > k] <- k - 1
+  arrow_df$arrow_end[arrow_df$arrow_end == 0] <- 2
+
 
   # Keep track of the dual process A(t)
   # Probability just need the last row of it.
@@ -34,8 +52,8 @@ vm_dual_sim <- function(n = 4, pretty = FALSE, delta = .1) {
      res[i, ] <- res[i - 1,]
      dual_res[i, ] <- dual_res[i - 1,]
    }
-    res[i, arrow_end[i]] <- res[i,event_i[i]]
-    dual_res[i, arrow_end[i]] <- dual_res[i, event_i[i]]
+    res[i, arrow_df$arrow_end[i]] <- res[i,arrow_df$event_i[i]]
+    dual_res[i, arrow_df$arrow_end[i]] <- dual_res[i, arrow_df$event_i[i]]
   }
 
   # Calculate the most switches for the dual process A(t)
@@ -46,7 +64,7 @@ vm_dual_sim <- function(n = 4, pretty = FALSE, delta = .1) {
   j <- 1
   x <- max_switches
   xend <- max_switches
-  y <- max(event_time) + delta
+  y <- max(arrow_df$event_time) + delta
   for(i in nrow(arrow_df):1) {
     if(arrow_df[i, "arrow_end"] == xend) {
       yend <- arrow_df[i, "event_time"]
@@ -68,8 +86,8 @@ vm_dual_sim <- function(n = 4, pretty = FALSE, delta = .1) {
 
   res_df <-  data.frame(res)
   colnames(res_df) <- 1:ncol(res_df)
-  res_df$event_time <- c(0, event_time)
-  res_df$event_end <- c(event_time, max(event_time) + delta)
+  res_df$event_time <- c(0, arrow_df$event_time)
+  res_df$event_end <- c(arrow_df$event_time, max(arrow_df$event_time) + delta)
   res_long <- gather(res_df, x, value, `1`:`9`)
   res_long$x <- as.integer(res_long$x)
 
@@ -89,19 +107,19 @@ vm_dual_sim <- function(n = 4, pretty = FALSE, delta = .1) {
   # Could simplify this a bit but seems more complicated
   lines_df <- data.frame(y = numeric(), yend = numeric(), x = integer(), xend = integer(), color = integer())
   for(j in 1:k) {
-   lines_df[j, ] <- c(y = 0, yend = event_time[1], x = j, xend = j, color = res[1, j])
+   lines_df[j, ] <- c(y = 0, yend = arrow_df$event_time[1], x = j, xend = j, color = res[1, j])
   }
 
   for(i in 2:event_n) {
    tmp_lines <- data.frame(y = numeric(), yend = numeric(), x = integer(), xend = integer(), color = integer())
    for(j in 1:k) {
-     tmp_lines[j, ] <- c(y = event_time[i - 1], yend = event_time[i], x = j, xend = j, color = res[i - 1, j])
+     tmp_lines[j, ] <- c(y = arrow_df$event_time[i - 1], yend = arrow_df$event_time[i], x = j, xend = j, color = res[i - 1, j])
    }
    lines_df <- rbind(lines_df, tmp_lines)
   }
 
   for(j in 1:k) {
-   tmp_lines[j, ] <- c(y = event_time[event_n], yend = event_time[event_n] + delta, x = j, xend = j, color = res[event_n, j])
+   tmp_lines[j, ] <- c(y = arrow_df$event_time[event_n], yend = arrow_df$event_time[event_n] + delta, x = j, xend = j, color = res[event_n, j])
   }
 
   lines_df <- rbind(lines_df, tmp_lines)
@@ -113,10 +131,14 @@ vm_dual_sim <- function(n = 4, pretty = FALSE, delta = .1) {
                 lineend = "round", linejoin = "bevel") +
    # Plot the line segements
    geom_segment(data = lines_df, aes(x = x, xend = xend, y = y, yend = yend, color = as.factor(color))) +
-   geom_text(data = dual_df, aes(x = x, y = max(event_time) + .2, label = dual)) +
+   # Text for dual process
+   geom_text(data = dual_df, aes(x = x, y = max(arrow_df$event_time) + .2, label = dual)) +
+   # Annotate the dual process
+   annotate("text", x =  k + 1, y = max(arrow_df$event_time) + 2 * delta, label = "A(t)") +
    # Show the dual process at finish time
    scale_x_continuous("", breaks = seq_len(k), labels = x_indices) +
    scale_y_continuous(expand = expansion(mult = c(0, delta))) +
+   coord_cartesian(xlim = c(1, k),clip = "off") +
    theme_minimal(base_size = 18) +
    theme_minimal() +
    theme(
@@ -124,7 +146,8 @@ vm_dual_sim <- function(n = 4, pretty = FALSE, delta = .1) {
     axis.title.y = element_blank(),
     axis.text.y = element_blank(),
     panel.grid = element_blank(),
-    legend.title = element_blank()
+    legend.title = element_blank(),
+    plot.margin = margin(0, 1.5, 0, 0, "cm")
     )
 
   example_path_plot <-
@@ -142,9 +165,9 @@ vm_dual_sim <- function(n = 4, pretty = FALSE, delta = .1) {
    geom_segment(data = dual_example_df,
                 aes(x = x, xend = xend, y = y, yend = yend)) +
    # Show the dual process at finish time
-   geom_text(data = dual_df, aes(x = x, y = max(event_time) + 2 * delta, label = dual)) +
+   geom_text(data = dual_df, aes(x = x, y = max(arrow_df$event_time) + 2 * delta, label = dual)) +
    # Annotate the dual process
-   annotate("text", x =  k + 1, y = max(event_time) + 2 * delta, label = "A(t)") +
+   annotate("text", x =  k + 1, y = max(arrow_df$event_time) + 2 * delta, label = "A(t)") +
    scale_x_continuous("", breaks = seq_len(k), labels = x_indices) +
    scale_y_continuous(expand = expansion(mult = c(0, delta))) +
    coord_cartesian(xlim = c(1, k),clip = "off") +
@@ -155,14 +178,16 @@ vm_dual_sim <- function(n = 4, pretty = FALSE, delta = .1) {
     axis.title.y = element_blank(),
     axis.text.y = element_blank(),
     panel.grid = element_blank(),
-    legend.title = element_blank()
+    legend.title = element_blank(),
+    plot.margin = margin(0, 1.5, 0, 0, "cm")
    )
 
   list(dual_plot = dual_plot, voter_plot = voter_plot, example_path_plot = example_path_plot)
 }
 
-set.seed(135)
-v <- vm_dual_sim(n = 5)
-v$example_path_plot
-v$dual_plot
+set.seed(13)
+v <- vm_dual_sim(n = 4, pretty = TRUE)
+g1 <- v$dual_plot
+g2 <- v$example_path_plot
 
+ggarrange(g1, g2, nrow = 1, common.legend = TRUE, legend="bottom")
