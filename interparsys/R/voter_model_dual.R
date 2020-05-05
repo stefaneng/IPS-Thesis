aa8bf6a219136bc802e0579569a834439b4014ce
Stefan Eng <stefaneng13@gmail.com>
2020-05-05 16:39:50 +0200

    Voter model dual figure

--- interparsys/R/voter_model_dual.R
+++ interparsys/R/voter_model_dual.R
@@ -190,4 +190,7 @@ v <- vm_dual_sim(n = 4, pretty = TRUE)
 g1 <- v$dual_plot
 g2 <- v$example_path_plot
 
+png(here("figures/voter_model_dual.png"))
 ggarrange(g1, g2, nrow = 1, common.legend = TRUE, legend="bottom")
+dev.off()
+
